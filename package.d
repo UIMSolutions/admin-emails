730a7c04b4a376a8d200d2a95dca4cc702f12e90
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-26 14:20:09 +0200

    Update

--- package.d
+++ package.d
@@ -8,5 +8,7 @@ public { // required uim libraries
 
 public { // admin-emails libraries
   import admins.emails.controllers;
+  import admins.emails.helpers;
+  import admins.emails.tests;
   import admins.emails.views;
 }
