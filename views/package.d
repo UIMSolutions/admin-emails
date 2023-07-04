60769d68c7a625938e5fc8e712601410a7d62a43
UIMSolutions <ui.manufaktur@gmail.com>
2023-07-04 11:37:40 +0200

    Initial update

--- views/package.d
+++ views/package.d
@@ -1,1 +1,35 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel, mailto:ons@sicherheitsschmiede.de                                                      
+**********************************************************************************************************/
 module admins.emails;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+	import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.bootstrap;
+  import uim.html;
+  import uim.oop;
+  import uim.models;
+
+  import uim.web;
+  import uim.mvc;
+  import uim.admins;
+
+  import web.controls;
+
+  import langs.javascript;
+}
+
+public {
+    import admin.emails.controllers;
+    import admin.emails.views;
+}
